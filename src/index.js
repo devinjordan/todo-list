9cac25e1ac42394c3cfa95f9a27f26880d94e5a7
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-03-29 18:41:01 -0400

    Feat: Task validation

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 class Task {
-  constructor(content, dueDate = new Date(), priority = 0, status = false) {
-    this.content = content;
+  constructor(description, dueDate = new Date(), priority = 0, status = false) {
+    this.Description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.status = status;
   };
 
-  changeContent(newContent) {
-    if (newContent === '') {
-      throw Error('Content must not be a blank string.');
-    }
-    this.content = newContent;
+  changeDescription(newDescription) {
+    Validation.validateDescription(newDescription);
+    this.Description = newDescription;
   };
 
   changeFullDate(year, month, day) {
-    const newDate = new Date(year, month - 1, day);
-    if (newDate < Date.now()) {
-      throw Error('Date may not be in the past');
-    }
-    this.dueDate = newDate;
+    Validation.validateDate(year, month, day);
+    this.dueDate = new Date(year, month - 1, day);
   }
 
   updatePriority(newPriority) {
@@ -31,17 +26,31 @@ class Task {
   markIncomplete() {
     this.status = false;
   };
-}
+};
+
+class Validation {
+  static validateDescription(newDescription) {
+    if (newDescription === '') {
+      throw Error('Include a description in the task.');
+    };
+  };
+
+  static validateDate(year, month, day) {
+    const newDate = new Date(year, month - 1, day);
+    if (newDate < Date.now()) {
+      throw Error('Date may not be in the past.');
+    };
+  };
+};
 
 
 const clean = new Task('Clean the house');
-clean.status = false;
 
 console.log(clean);
 
 clean.markComplete();
 clean.changeFullDate(2025, 2, 20);
-clean.changeContent('Take out the trash');
+clean.changeDescription('Take out the trash');
 clean.updatePriority(2);
 
 console.log(clean);
