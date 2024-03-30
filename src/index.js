cca2a75f3b3ae37a0f8382162a67b0652b325c7e
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-03-30 16:02:36 -0400

    Complete commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import createList from "./modules/list.js"; 
 import createTask from "./modules/task.js";
+import modifyTask from "./modules/modify.js";
+
+// TODO: DOM manipulation
 
 const myList = createList('My List');
 
@@ -9,79 +12,14 @@ const due = createTask('Due', new Date(2026, 5, 21), 1);
 const today = createTask('Today', new Date(2026, 5, 21), 1);
 
 
-
 myList.addTask(work);
 myList.addTask(play);
 myList.addTask(due);
 myList.addTask(today);
 
+console.log(work.description);
+work.modify.description('Work on project');
+console.log(work.description);
+
 console.log(myList.name);
 console.table(myList.taskList);
-
-
-// Class style orignial code
-
-// class Task {
-//   constructor(description, dueDate = new Date(), priority = 0, status = false) {
-//     this.description = description;
-//     this.dueDate = dueDate;
-//     this.priority = priority;
-//     this.status = status;
-//   };
-
-//   changeDescription(newDescription) {
-//     Validation.validateDescription(newDescription);
-//     this.description = newDescription;
-//   };
-
-//   changeFullDate(year, month, day) {
-//     Validation.validateDate(year, month, day);
-//     this.dueDate = new Date(year, month - 1, day);
-//   }
-
-//   updatePriority(newPriority) {
-//     this.priority = newPriority;
-//   };
-
-//   toggleStatus() {
-//     this.status = !this.status;
-//   };
-// };
-
-// class Validation {
-//   static validateDescription(newDescription) {
-//     if (newDescription === '') {
-//       throw Error('Include a description in the task.');
-//     };
-//   };
-
-//   static validateDate(year, month, day) {
-//     const newDate = new Date(year, month - 1, day);
-//     if (newDate < Date.now()) {
-//       throw Error('Date may not be in the past.');
-//     };
-//   };
-// };
-
-// class List {
-//   constructor() {
-//     this.tasks = [];
-//   };
-
-//   addTask(task) {
-//     this.tasks.push(task);
-//   };
-
-//   removeTask(index) {
-//     this.tasks.splice(index, 1);
-//   };
-
-//   populate() {
-//     let taskList = [];
-//     for (let i = 0; i < this.tasks.length; i++) {
-//       taskList.push(this.tasks[i]);
-//     };
-
-//     return taskList;
-//   };
-// };
