39ec33eee393488a07cff162a03732c853dce17f
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-04-01 18:55:57 -0400

    Update index.html and style.css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './style.css';
 // TODO: DOM manipulation
 const listsDiv = document.getElementById('lists');
 const tasksDiv = document.getElementById('tasks');
+const title = document.getElementById('title');
 
 createList('Default List');
 const newTask = createTask('Groceries', new Date(2021, 8, 1), 1);
@@ -13,26 +14,36 @@ lists[0].addTask(newTask);
 
 const renderLists = () => {
   listsDiv.innerHTML = '';
+
+  const addListButton = document.createElement('button');
+  addListButton.textContent = '+ Add List';
+  addListButton.id = 'add-list-button';
+  listsDiv.appendChild(addListButton);
+
+
   for (let list of lists) {
     const listButton = document.createElement('button');
     listButton.classList.add('list-button');
     listButton.textContent = list.name;
     listButton.addEventListener('click', () => {
+      title.textContent = list.name;
       // TODO: Render tasks
+      tasksDiv.innerHTML = '';
+      for (let task of list.taskList) {
+        const taskDiv = document.createElement('div');
+        taskDiv.classList.add('task');
+        
+        tasksDiv.appendChild(taskDiv);
+      }
     });
     listsDiv.appendChild(listButton);
   };
-
-  const addListButton = document.createElement('button');
-  addListButton.textContent = '+';
 
   addListButton.addEventListener('click', () => {
     const listName = prompt('Enter the name of the new list:');
     createList(listName);
     renderLists();
   });
-
-  listsDiv.appendChild(addListButton);
 
 };
 
